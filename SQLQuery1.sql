34c714c2386af9326486af9d5b0b5dd037320d55
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-11-21 09:30:53 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+--insert query
+insert dbo.tbl_studentmanagement (Student_Name,Student_usn,Student_email)values('std0003','abd','add@gmail.com')
---
+++ SQLQuery2.sql
@@ -0,0 +1,2 @@
+--insert query
+insert dbo.tbl_studentmanagement (Student_Name,Student_usn,Student_email)values('std0005','rst','rst@gmail.com')
